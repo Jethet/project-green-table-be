b6b563399c7d4d22d7977f015fd93b060fb8b454
Jethet <henriette.hettinga@gmail.com>
2020-03-09 16:36:35 +0100

    New User model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Table = require("./Table");
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  imageURL: String,
+  city: String,
+  preferences: {
+    vegetarian: Boolean,
+    vegan: Boolean,
+    glutenFree: Boolean,
+    nonAlcohol: Boolean
+  },
+  table: [{ type: Schema.Types.ObjectId, ref: "Table" }]
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

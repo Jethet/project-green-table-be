3bc0bc8b04ac62259d8995ad786dde56ca468a92
Jethet <henriette.hettinga@gmail.com>
2020-03-09 16:27:02 +0100

    Production deployment v1

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
+require("dotenv").config();
 
-const authRouter = require('./routes/authRouter');
-const profileRouter = require('./routes/profileRouter');
-const tableRouter = require('./routes/tableRouter');
-
+const authRouter = require("./routes/authRouter");
+const profileRouter = require("./routes/profileRouter");
+const tableRouter = require("./routes/tableRouter");
 
 // MONGOOSE CONNECTION
 mongoose
   .connect(process.env.MONGODB_URI, {
     keepAlive: true,
     useNewUrlParser: true,
-    reconnectTries: Number.MAX_VALUE,
+    reconnectTries: Number.MAX_VALUE
   })
-  .then( () => console.log(`Connected to database`))
-  .catch( (err) => console.error(err));
-
+  .then(() => console.log(`Connected to database`))
+  .catch(err => console.error(err));
 
 // EXPRESS SERVER INSTANCE
 const app = express();
-
 
 // CORS MIDDLEWARE SETUP
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN],
-  }),
+    origin: [process.env.PUBLIC_DOMAIN,
+      "https://project-the-green-table.herokuapp.com"]
+  })
 );
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -49,49 +47,52 @@ app.use(
   session({
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60 * 60, // 60 days
+      ttl: 24 * 60 * 60 * 60 // 60 days
     }),
     secret: process.env.SECRET_SESSION,
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  }),
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
 );
 
 // MIDDLEWARE
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTER MIDDLEWARE
-app.use('/auth', authRouter);
-app.use('/profile', profileRouter);
-app.use('/table', tableRouter);
+app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
+app.use("/table", tableRouter);
 
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-// 404 
+// 404
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404).json({ code: 'not found' });
+  res.status(404).json({ code: "not found" });
 });
 
 // ERROR HANDLING
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error("ERROR", req.method, req.path, err);
 
-  // only send the error if the error ocurred before sending the response 
+  // only send the error if the error ocurred before sending the response
   // (don't try to send the response after it has already been sent)
   if (!res.headersSent) {
-    const statusError = err.status || '500';
+    const statusError = err.status || "500";
     res.status(statusError).json(err);
   }
 });
-
 
 module.exports = app;
---
+++ public/precache-manifest.7aae7e38bbc774d80c2b6c92867a7d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795831cad65559c44c3f9ac8175f9ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1893be3f48e575d9b0db",
+    "url": "/static/css/main.5a7a9852.chunk.css"
+  },
+  {
+    "revision": "04e6b8f9a30b1dd48e01",
+    "url": "/static/js/2.d3236a17.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d3236a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1893be3f48e575d9b0db",
+    "url": "/static/js/main.8416cdc7.chunk.js"
+  },
+  {
+    "revision": "5dde3ecc687698d22105",
+    "url": "/static/js/runtime-main.22b866d5.js"
+  }
+]);

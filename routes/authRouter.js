a75f08f2426003b27ea53919a3bc73c6c66b738b
Jethet <henriette.hettinga@gmail.com>
2020-03-09 15:32:22 +0100

    Deploy to heroku

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,19 +7,16 @@ const User = require("../models/User");
 const Table = require("../models/Table");
 
 // HELPER FUNCTIONS
-const {
-  isLoggedIn,
-  isNotLoggedIn,
-  validationLogin
-} = require("../helpers/middlewares");
+const { isLoggedIn, isNotLoggedIn, validationLogin } = require("../helpers/middlewares");
 
 // POST    '/auth/signup'
-authRouter.post('/signup', isNotLoggedIn, validationLogin, async (req, res, next) => {
+authRouter.post("/signup", isNotLoggedIn, validationLogin, async (req, res, next) => {
   const { username, password } = req.body;
 
-  try {																									 // projection
-    const usernameExists = await User.findOne({ username }, 'username');
-    
+  try {
+    // projection
+    const usernameExists = await User.findOne({ username }, "username");
+
     if (usernameExists) return next(createError(400));
     else {
       const salt = bcrypt.genSaltSync(saltRounds);
@@ -30,56 +27,47 @@ authRouter.post('/signup', isNotLoggedIn, validationLogin, async (req, res, next
       newUser.password = "*";
       req.session.currentUser = newUser;
       res
-        .status(201)  //  Created
+        .status(201) //  Created
         .json(newUser);
     }
-  } 
-  catch (error) {
+  } catch (error) {
     next(createError(error));
   }
-},
-);
+});
 
 // POST '/auth/login'
-authRouter.post('/login', isNotLoggedIn, validationLogin, async (req, res, next) => {
+authRouter.post("/login", isNotLoggedIn, validationLogin, async (req, res, next) => {
   const { username, password } = req.body;
   try {
-    const user = await User.findOne({ username }) ;
+    const user = await User.findOne({ username });
     if (!user) {
       next(createError(404));
-    } 
-    else if (bcrypt.compareSync(password, user.password)) {
-      
-      user.password = '*';
+    } else if (bcrypt.compareSync(password, user.password)) {
+      user.password = "*";
       req.session.currentUser = user;
-      res
-        .status(200)
-        .json(user);
-    //return;	 			TODO - remove from the notes
-    } 
-    else {
-      next(createError(401));	// Unauthorized
+      res.status(200).json(user);
+      // return; TODO - remove from the notes
+    } else {
+      next(createError(401)); // Unauthorized
     }
-  } 
-  catch (error) {
+  } catch (error) {
     next(createError(error));
   }
-},
-);
+});
 
 // POST '/auth/logout'
-authRouter.post('/logout', isLoggedIn, (req, res, next) => {
+authRouter.post("/logout", isLoggedIn, (req, res, next) => {
   req.session.destroy();
   res
-    .status(204)  //  No Content
+    .status(204) //  No Content
     .send();
 });
 
 // GET '/auth/me' - used by Reach frontend when React app loads for the first time
-authRouter.get('/me', isLoggedIn, (req, res, next) => {
+authRouter.get("/me", isLoggedIn, (req, res, next) => {
   const currentUserSessionData = req.session.currentUser;
-  currentUserSessionData.password = '*';
-  
+  currentUserSessionData.password = "*";
+
   res.status(200).json(currentUserSessionData);
 });
 
